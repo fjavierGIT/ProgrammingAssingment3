270a19a8a6a2159c1ebabc57d0be8c068774cdee
fjavierGIT <fjangoy@gmail.com>
2017-02-10 19:27:24 +0100

    Improved subsetting

--- rankhospital.R
+++ rankhospital.R
@@ -2,6 +2,7 @@ rankhospital <- function(state, outcome, num = "best") {
   ## Read outcome data
   myData <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings="Not Available", stringsAsFactors=FALSE)
   outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23) #outcomes names list
+  myData <- na.omit(myData[, c(2, 7, outcomes[outcome])])
   
   ## Check that state and outcome are valid
   if(!state %in% myData$State) {
@@ -13,8 +14,8 @@ rankhospital <- function(state, outcome, num = "best") {
   
   ## Return hospital name in that state with lowest outcome rate
   mynewData <- subset(myData, myData$State==state,na.rm=TRUE)  #Select by State
-  mynewData[,outcomes[outcome]] <- as.numeric(mynewData[,outcomes[outcome]]) #Coerce outcome column to numeric
-  mynewData <- na.omit(mynewData[order(mynewData[,outcomes[outcome]],mynewData[,2]),]) #Sort by Outcome&Name
+  mynewData[,3] <- as.numeric(mynewData[,3]) #Coerce outcome column to numeric
+  mynewData <- na.omit(mynewData[order(mynewData[,3],mynewData[,1]),]) #Sort by Outcome&Name
   
   ## Determine num for 'best' and 'worst'
   if (is.character(num) == TRUE) {
@@ -22,9 +23,9 @@ rankhospital <- function(state, outcome, num = "best") {
       num = 1
     }
     else if (num == "worst") {
-      num = length(mynewData[, outcomes[outcome]])
+      num = length(mynewData[, 2])
     }
   }
   ## Rated value
-  return(c(mynewData[num,2]))
+  return(c(mynewData[num,1]))
 }
